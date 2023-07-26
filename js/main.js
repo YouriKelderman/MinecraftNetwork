d14b22473d38248a9723f87c00849c50b8885afa
Eigenaar <youri10jaar@hotmail.com>
2023-07-26 17:33:35 +0200

    start of games section

--- js/main.js
+++ js/main.js
@@ -36,7 +36,23 @@ function windowScroll() {
     } else {
         navbar.classList.remove("active")
     }
+    
 }
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+reveal();
+window.addEventListener("scroll", reveal);
 
 function dotSetup() {
     for (let i = 1; i <= images.length; i++) {
