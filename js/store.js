3383defdafce9897a82e7158dd2224e9e57b81d8
Eigenaar <youri10jaar@hotmail.com>
2023-07-28 15:56:24 +0200

    start of coins section

--- js/store.js
+++ js/store.js
@@ -4,6 +4,8 @@ window.addEventListener('load', init);
 let coinsSpan;
 let costSpan;
 let range;
+let flood;
+let parent;
 let prices = [
     [60, 1.49],
     [300, 6.99],
@@ -17,6 +19,8 @@ function init() {
     range = document.getElementById("coinRange");
     coinsSpan = document.getElementById("coins");
     costSpan = document.getElementById("cost");
+    parent = document.getElementById("amount")
+    flood = document.getElementById("flood");
     range.max = (prices.length -1).toString();
     sliderChange(0)
     range.value = 0;
@@ -25,4 +29,7 @@ function init() {
 function sliderChange(e) {
     coinsSpan.innerHTML = `Coins: ${prices[e][0]}`
     costSpan.innerHTML = `$${prices[e][1]}`;
+    let heightToUse = (e/prices.length) * parent.getBoundingClientRect().height +50;
+    console.log(heightToUse);
+    flood.style.height = `${heightToUse}px`;
 }
